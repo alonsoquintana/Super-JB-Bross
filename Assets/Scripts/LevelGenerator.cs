b404425c9e606abfa0b1bd425931924b6a5713bf
alonsoquintana <iaquintanapa@gmail.com>
2020-07-20 21:58:58 -0500

    Iniciando el tutorial 3

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelGenerator : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class LevelGenerator : MonoBehaviour{
+
+    public static LevelGenerator sharedInstance;
+
+    public List<LevelBlock> allTheLevelBlocks = new List<LevelBlock>();
+
+    public Transform levelStartPoint;
+
+    public List<LevelBlock> currentBlocks = new List<LevelBlock>();
+
+    private void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    public void AddLevelBlock()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveOldestLevelBlock()
     {
-        
+
     }
+
+    public void RemoveAllTheBlocks()
+    {
+
+    }
+
+    public void GeneratorInitialBlocks()
+    {
+
+    }
+
+        
 }
