d2467403a9e6f734c3c3306c05680ddd43ecbfa4
Deutshland00- <bpaucarm@outlook.com>
2020-08-15 21:28:47 -0500

    En el tutorial 3

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour{
+
+    public Transform target;
+    public Vector2 offset = new Vector2(0.1f, 10f);
+    public float dampTime = 0.3f;
+    public Vector3 velocity = Vector3.zero;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+        
+    void Update()
+    {
+        Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+        Debug.Log(point);
+    }   
+}
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -17,9 +17,34 @@ public class LevelGenerator : MonoBehaviour{
         sharedInstance = this;
     }
 
+    private void Start()
+    {
+        AddLevelBlock();
+        AddLevelBlock();
+        AddLevelBlock();
+    }
+
     public void AddLevelBlock()
     {
-        
+        int randomIndex = Random.Range(0, allTheLevelBlocks.Count);
+
+        LevelBlock currentBlock = (LevelBlock)Instantiate(allTheLevelBlocks[randomIndex]);
+        currentBlock.transform.SetParent(this.transform, false);
+
+        Vector3 spawPosition = Vector3.zero;
+
+        if (currentBlocks.Count == 0)
+        {
+            spawPosition = levelStartPoint.position;
+        }else
+        {
+            spawPosition = currentBlocks[currentBlocks.Count - 1].exitPoint.position;
+        }
+
+        Vector3 correction = new Vector3(spawPosition.x-currentBlock.startPoint.position.x, spawPosition.y-currentBlock.startPoint.position.y, 0);
+              
+        currentBlock.transform.position = correction;
+        currentBlocks.Add(currentBlock);
     }
 
     public void RemoveOldestLevelBlock()
